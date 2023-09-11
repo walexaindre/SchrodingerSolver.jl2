2d88fa1bab8475aad78d4d8e119cc72df0cf3c51
walexaindre <walexaindre@hotmail.com>
2023-09-11 19:53:25 -0400

    Project Structuring

---
+++ lib/index.jl
@@ -0,0 +1,7 @@
+#Base Types and functions
+include("AbstractMesh/AbstractMesh.jl")
+include("ExecutionBackend/ExecutionBackend.jl")
+include("ShrodingerPDE/ShrodingerPDE.jl")
+include("SpaceTimeGrid/SpaceTimeGrid.jl")
+
+#Methods
